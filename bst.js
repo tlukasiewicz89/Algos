9b58b8aaad747837988af151e4221eaef71e12f1
Thomas Lukasiewicz <53915559+tlukasiewicz89@users.noreply.github.com>
2022-10-07 10:22:17 -0600

    updated BST

--- bst.js
+++ bst.js
@@ -1,34 +1,50 @@
 class BST {
     constructor() {
         this.root = null;
+        this.insert = this.insert.bind(this);
+        this.find = this.find.bind(this);
     }
     insert(value) {
         let newNode = new Node(value);
         if (this.root === null) {
             this.root = newNode;
             return this;
-        } else {
-            let curr = this.root;
-            while (true) {
-                if (value < curr.value) {
-                    if (curr.left === null) {
-                        curr.left = newNode;
-                        return this;
-                    } else {
-                        curr = curr.left;
-                    }
-                } else {
-                    if (curr.right === null) {
-                        curr.right = newNode;
-                        return this;
-                    } else {
-                        curr = curr.right;
-                    }
-                }
+        } 
+        let curr = this.root;
+        while (true) {
+            if (value === curr.value) return undefined;
+            if (value < curr.value) {
+                if (curr.left === null) {
+                    curr.left = newNode;
+                    return this;
+                } 
+                curr = curr.left;
+            } else {
+                if (curr.right === null) {
+                    curr.right = newNode;
+                    return this;
+                } 
+                curr = curr.right;
             }
         }
-
-
+    }
+    find(value) {
+        if (this.root === null) return undefined;
+        let curr = this.root;
+        while(true) {
+            if (curr.value === value) {
+                return curr;
+            } 
+            if (value < curr.value) {
+                if (curr.left) {
+                    curr = curr.left;
+                } else return undefined;
+            } else {
+                if (curr.right) {
+                    curr = curr.right;
+                } else return undefined;
+            }
+        }
     }
 }
 
@@ -44,6 +60,14 @@ let tree = new BST();
 tree.insert(10);
 tree.insert(5);
 tree.insert(9);
+tree.insert(7);
+console.log(tree.find(9))
+console.log(tree.insert(10));
 
 console.log(tree.root.left);
 
+function findClosestValueInBst(tree, target, obj = {nodeVale: Inifinity, distance: Infinity) {
+  console.log(obj);
+}
+
+console.log(findClosestValueInBst(tree, 4))
